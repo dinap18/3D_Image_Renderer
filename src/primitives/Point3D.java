0a1ae2fe4585acd812eb9136d6d00a9152e7a000
Dina Pinchuck <dp18@msn.com>
2020-03-18 14:35:21 +0200

    a

--- src/primitives/Point3D.java
+++ src/primitives/Point3D.java
@@ -1,4 +1,120 @@
 package primitives;
-
+import static java.lang.Math.*;
+/**
+ *  Point#d basic point with coordinates in X axis, Y axis and Z axis
+ */
 public class Point3D {
+
+
+        /**
+         * Coordinates of Point3D
+         */
+        Coordinate _x;
+        Coordinate _y;
+        Coordinate _z;
+
+    public void set_x(Coordinate _x) {
+        this._x = _x;
+    }
+
+    public void set_y(Coordinate _y) {
+        this._y = _y;
+    }
+
+    public void set_z(Coordinate _z) {
+        this._z = _z;
+    }
+
+    public final static Point3D ZERO = new Point3D(0.0,0.0,0.0);
+        /**
+         *
+         * @param _x x coordinate
+         * @param _y y coordinate
+         * @param _z z coordinate
+         */
+        public Point3D(Coordinate _x, Coordinate _y, Coordinate _z) {
+            this._x = _x;
+            this._y = _y;
+            this._z = _z;
+        }
+
+        public Point3D(double _x, double _y, double _z) {
+            this(new Coordinate(_x),new Coordinate(_y),new Coordinate(_z));
+        }
+
+    /**
+     * copy constructor
+     * @param p
+     */
+    public Point3D(Point3D p) {
+        new Point3D(new Coordinate(p._x),new Coordinate(p._y),
+        _z = new Coordinate(p._z));
+    }
+
+    /**
+         *
+         * @return new Coordinate based on _x
+         */
+        public Coordinate get_x() {
+            return new Coordinate(_x);
+        }
+
+        public Coordinate get_y() {
+            return new Coordinate(_y);
+        }
+
+        public Coordinate get_z() {
+            return new Coordinate(_z);
+        }
+
+        /**
+         * TODO
+         * @param o
+         * @return
+         */
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Point3D point3D = (Point3D) o;
+            return _x.equals(point3D._x) &&
+                    _y.equals(point3D._y) &&
+                    _z.equals(point3D._z);
+        }
+
+        @Override
+        public String toString() {
+            return "(" +
+                    _x +
+                    ", " + _y +
+                    ", " + _z +
+                    ')';
+        }
+        public Vector subtract(Point3D p) {
+            return new Vector(new Point3D(
+                    p._x.get()- this._x.get(),
+                    p._y.get()- this._y.get(),
+                    p._z.get()- this._z.get()
+            ));
+        }
+        public Point3D add(Vector v)
+        {
+            return new Point3D(
+                   v._head._x.get()+ this._x.get(),
+                    v._head._y.get()+ this._y.get(),
+                    v._head._z.get()+ this._z.get());
+
+        }
+        public double distanceSquared(Point3D p)
+        {
+            return (_x.get()-(p._x).get())*(_x.get()-(p.get_x()).get())+
+                    (_y.get()-(p.get_y()).get())*(_y.get()-(p.get_y()).get())
+                    +(_z.get()-(p._z).get())*(_z.get()-(p._z).get());
+        }
+        public double distance(Point3D p)
+        {
+            return Math.sqrt((_x.get()-(p._x).get())*(_x.get()-(p.get_x()).get())+
+                    (_y.get()-(p.get_y()).get())*(_y.get()-(p.get_y()).get())
+                    +(_z.get()-(p._z).get())*(_z.get()-(p._z).get()));
+        }
 }
--- src/primitives/Vector.java
+++ src/primitives/Vector.java
@@ -1,4 +1,100 @@
 package primitives;
 
+import java.util.Objects;
+
 public class Vector {
+    Point3D _head;
+
+    public Vector(Point3D _head) {
+        this._head = _head;
+    }
+  /*  public Vector(Point3D _p1, Point3D _p2) {
+        this._head= _p2.subtract(_p1);
+    }*/
+
+    public Vector(double x,double y,double z)
+    {
+        this._head._x=new Coordinate(x);
+        this._head._y=new Coordinate(y);
+        this._head._z=new Coordinate(z);
+    }
+    public Vector(Coordinate x,Coordinate y,Coordinate z)
+    {
+
+    }
+    public Vector(Vector v)
+    {
+        new Vector(v._head._x,v._head._y,v._head._z);
+    }
+    public Point3D get_head() {
+        return new Point3D(_head._x,_head._y,_head._z);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Vector vector = (Vector) o;
+        return _head.equals(vector._head);
+    }
+
+
+    public Vector subtract(Vector v)
+    {
+      return this._head.subtract(v._head);
+
+    }
+    public Vector add(Vector v)
+    {
+        return new Vector( this._head.add(v));
+    }
+    public Vector scale(double d)
+    {
+        return new Vector(this._head._x.get()*d,this._head._y.get()*d,this._head._z.get()*d);
+    }
+    public double dotProduct(Vector v)
+    {
+        double dp;
+        dp = (this._head.get_x().get()*(v._head.get_x().get()))
+                + (this._head.get_y().get()*(v._head.get_y().get()))
+                + (this._head.get_z().get()*(v._head.get_z().get()));
+        return dp;
+    }
+    public Vector crossProduct(Vector v)
+    {
+        double x = (this._head.get_y().get()*(v._head.get_z().get()))-(this._head.get_z().get()*(v._head.get_y().get()));
+        double y = (this._head.get_z().get()*(v._head.get_x().get()))-(this._head.get_x().get()*(v._head.get_z().get()));
+        double z = (this._head.get_x().get()*(v._head.get_y().get()))-(this._head.get_y().get()*(v._head.get_x().get()));
+        Coordinate x1 = new Coordinate(x);
+        Coordinate y1 = new Coordinate(y);
+        Coordinate z1 = new Coordinate(z);
+        Point3D p3D = new Point3D(x1,y1,z1);
+        Vector cp = new Vector(p3D);
+        return cp;
+    }
+    public double lengthSquared()
+    {
+        return this.length()*this.length();
+    }
+    public double length()
+    {
+        double lgth;
+        lgth = Math.pow(this._head.get_x().get(),2) +
+                Math.pow(this._head.get_y().get(),2) +
+                Math.pow(this._head.get_z().get(), 2);
+        lgth = Math.sqrt(lgth);
+        return lgth;
+    }
+    public Vector normalize()
+    {
+        double l= this.length();
+        this._head=((this._head.get_x().get()/l));
+        this._head.get_y()=(this._head.get_y().get()/l);
+        this._head.get_z()=(this._head.get_z().get()/l);
+    }
+    public Vector normalized()
+    {
+
+    }
+
 }
